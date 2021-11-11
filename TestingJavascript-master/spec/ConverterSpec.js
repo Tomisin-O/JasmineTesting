1ad44a58a3f800deba073ce86c3fe417cc75a5d8
Tomisin-O <onasanyatomisin@gmail.com>
2021-11-11 19:12:56 +0000

    Volume and Distance Converter

--- TestingJavascript-master/spec/ConverterSpec.js
+++ TestingJavascript-master/spec/ConverterSpec.js
@@ -16,3 +16,28 @@
     });
   });
 
+  describe("Weight", function() {
+    it("Pounds to Kilos", function() {
+      var result = convertPoundstoKilos(60);
+
+      expect(result).toEqual('27.22');
+    });
+  });
+
+  describe("Volume", function() {
+    it("Litre to Gallons", function() {
+      var result = convertLitretoGallons(3);
+
+      expect(result).toEqual('0.66');
+    });
+  });
+
+  describe("Distance", function() {
+    it("Miles to Km", function() {
+      var result = convertMilestoKm(5);
+
+      expect(result).toEqual('8');
+    });
+  });
+
+
--- TestingJavascript-master/src/Converter.js
+++ TestingJavascript-master/src/Converter.js
@@ -10,3 +10,21 @@ function convertCelsiusToFahrenheit(temperature) {
   let fahrenheit = ((temperature * 1.8) + 32);
   return fahrenheit.toFixed(1) ;
 }
+
+function convertPoundstoKilos(pound) {
+  //conversion code
+  let kilos = pound * 0.4536;
+  return kilos.toFixed(2) ;
+}
+
+function convertLitretoGallons(litre) {
+  //conversion code
+  let gallons = litre * 0.22;
+  return gallons.toFixed(2) ;
+}
+
+function convertMilestoKm(miles) {
+  //conversion code
+  let Km = miles * 1.609;
+  return Km.toFixed(0);
+}
